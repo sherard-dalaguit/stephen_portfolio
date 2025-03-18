2dfdfe0d2382956a2cda45bf444f4ad563c8cc99
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-03-18 12:45:00 -0700

    Update FlipWords duration, enhance Hero section layout, and add Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      <div className="w-screen">
+        <div className="absolute top-0 left-[50%] right-[50%] z-50">
+          <Navbar />
+        </div>
         <Hero />
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,43 +2,48 @@ import {TextGenerateEffect} from "@/components/ui/text-generate-effect";
 import {Spotlight} from "@/components/ui/spotlight-new";
 import {cn} from "@/lib/utils";
 import {FlipWords} from "@/components/ui/flip-words";
+import { IconAward } from "@tabler/icons-react";
 import {SparklesCore} from "@/components/ui/sparkles";
+import MagicButton from "@/components/ui/MagicButton";
 
 const Hero = () => {
 	const words = ["Full-Stack Developer", "Software Engineer", "Computer Science Student"];
 
 	return (
-		<div className="pb-20 pt-36">
+		<div className="w-screen min-h-screen pb-20 pt-36 dark:bg-black">
 			<Spotlight/>
 
-			<div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.075] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-				<div
-					className={cn(
-						"absolute inset-0",
-						"[background-size:80px_80px]",
-						"[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-						"dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-					)}
-				/>
-				{/* Radial gradient for the container to give a faded look */}
-				<div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-			</div>
+			{/*<div className="h-screen w-full dark:bg-black bg-white bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">*/}
+			{/*	<div*/}
+			{/*		className={cn(*/}
+			{/*			"absolute inset-0",*/}
+			{/*			// "[background-size:80px_80px]",*/}
+			{/*			"[background-size:100px_100px]",*/}
+			{/*			"[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",*/}
+			{/*			"dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",*/}
+			{/*		)}*/}
+			{/*	/>*/}
+			{/*	/!* Radial gradient for the container to give a faded look *!/*/}
+			{/*	<div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>*/}
+			{/*</div>*/}
 
 			<div className="h-[70vh] flex justify-center relative z-10">
 				<div className="md:max-w-3xl lg:max-w-[70vw] flex flex-col items-center justify-center">
 					<TextGenerateEffect
-						className="text-center text-[56px] md:text-7xl lg:text-8xl"
+						className="text-center text-[44px] md:text-7xl lg:text-8xl xl:text-9xl"
 						words="Sherard Dalaguit"
 					/>
 
-					<div className="flex flex-row gap-2 text-sm md:text-lg lg:text-2xl">
-						<FlipWords words={words}/> |
+					<div className="flex flex-row gap-2 text-lg md:text-xl lg:text-2xl xl:text-3xl">
+						<div className="hidden sm:block">
+							<FlipWords words={words}/> |
+						</div>
 						<p className="text-[#d1b3ff] text-center ml-2.5 md:tracking-wider mb-4">
 							SWE & CS @ UC Irvine
 						</p>
 					</div>
 
-					<div className="w-[40rem] h-40 mt-5 relative">
+					<div className="w-[40rem] h-40 mt-4 relative">
 						{/* Gradients */}
 						<div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm"/>
 						<div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4"/>
@@ -61,11 +66,16 @@ const Hero = () => {
 								particleColor="#FFFFFF"
 							/>
 						</div>
-
 					</div>
+
+					<a href="/#projects">
+						<MagicButton title="Show my work" icon={<IconAward />} position="right" />
+					</a>
 
 				</div>
 			</div>
+
+			<p className="mt-500">hello</p>
 		</div>
 	)
 }
