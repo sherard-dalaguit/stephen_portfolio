44e616cf0ed2c8a97f43a3e97e8602842b1b2fcd
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-27 13:05:01 -0700

    add selfie as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sherard's Portfolio",
   description: "Modern Portfolio",
+  icons: {
+    icon: "/selfie.jpg",
+  }
 };
 
 export default function RootLayout({
