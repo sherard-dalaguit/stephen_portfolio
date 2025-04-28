857580161c5e69c260628d453533715c00202ee1
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-28 12:13:22 -0700

    updated margins/padding

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,8 +3,8 @@ import {projects} from "@/data";
 
 const Projects = () => {
 	return (
-		<div id="projects" className="max-w-screen-xl mx-auto w-full py-40 px-4">
-			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl">
+		<div id="projects" className="max-w-screen-xl mx-auto w-full py-30 px-4">
+			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl mb-20">
 				My <span className="text-[#d1b3ff]">Personal Projects</span>
 			</h1>
 			<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-12 gap-4 justify-items-stretch w-full">
