82522f589cf48cdc6242828211d7fa5111ebbb4b
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-28 12:45:45 -0700

    updated projects margins/padding

--- components/ProjectDetails.tsx
+++ components/ProjectDetails.tsx
@@ -21,7 +21,7 @@ export const ProjectItems = ({
 }) => {
 	return (
 		<CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-auto rounded-xl p-6 border">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border">
         <CardItem
           translateZ="50"
           className="text-xl md:text-2xl font-bold text-neutral-600 dark:text-white"
