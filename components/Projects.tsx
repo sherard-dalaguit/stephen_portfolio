bcabbb495596de76bc371d428f7e950269ac5eb8
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-28 12:47:08 -0700

    updated projects margins/padding

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
 			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl mb-20">
 				My <span className="text-[#d1b3ff]">Personal Projects</span>
 			</h1>
-			<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-12 gap-4 justify-items-center">
+			<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-12 gap-4 justify-items-stretch">
 				{projects.map(({id, title, subheader, des, img, iconLists, githubLink, deployedLink }) => (
 					<ProjectItems
 						key={id}
