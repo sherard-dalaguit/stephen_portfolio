348068f6519538bad52b6b4a38c1fc890137bfac
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-28 21:50:18 -0700

    updated projects margins/padding

--- components/ProjectDetails.tsx
+++ components/ProjectDetails.tsx
@@ -21,40 +21,42 @@ export const ProjectItems = ({
 }) => {
 	return (
 		<CardContainer className="inter-var">
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border">
-        <CardItem
-          translateZ="50"
-          className="text-xl md:text-2xl font-bold text-neutral-600 dark:text-white"
-        >
-					{title}
-        </CardItem>
+      <CardBody className="flex flex-col bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-6 border">
+        <div className="flex-1 flex flex-col">
+					<CardItem
+						translateZ="50"
+						className="text-xl md:text-2xl font-bold text-neutral-600 dark:text-white"
+					>
+						{title}
+					</CardItem>
 
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-neutral-500 text-md md:text-lg max-w-sm mt-2 dark:text-neutral-300"
-        >
-					{subheader}
-        </CardItem>
-				<CardItem
-          as="p"
-          translateZ="60"
-          className="text-sm max-w-sm mt-2"
-        >
-					{des}
-        </CardItem>
+					<CardItem
+						as="p"
+						translateZ="60"
+						className="text-neutral-500 text-md md:text-lg max-w-sm mt-2 dark:text-neutral-300"
+					>
+						{subheader}
+					</CardItem>
+					<CardItem
+						as="p"
+						translateZ="60"
+						className="text-sm max-w-sm mt-2"
+					>
+						{des}
+					</CardItem>
 
-        <CardItem translateZ="100" className="w-full mt-4">
-          <Image
-            src={img}
-            height="1000"
-            width="1000"
-            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem>
+					<CardItem translateZ="100" className="w-full mt-4">
+						<Image
+							src={img}
+							height="1000"
+							width="1000"
+							className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+							alt="thumbnail"
+						/>
+					</CardItem>
+				</div>
 
-        <div className="flex justify-between items-center mt-20">
+				<div className="flex justify-between items-center mt-auto">
           <CardItem
             translateZ={20}
             as={Link}
