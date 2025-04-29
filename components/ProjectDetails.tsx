14519656ad897d809cc56768f03fccaf542d5347
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-04-29 06:52:30 -0700

    updated projects margins/padding

--- components/ProjectDetails.tsx
+++ components/ProjectDetails.tsx
@@ -44,17 +44,17 @@ export const ProjectItems = ({
 					>
 						{des}
 					</CardItem>
-
-					<CardItem translateZ="100" className="w-full mt-4">
-						<Image
-							src={img}
-							height="1000"
-							width="1000"
-							className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-							alt="thumbnail"
-						/>
-					</CardItem>
 				</div>
+
+				<CardItem translateZ="100" className="w-full mb-10">
+					<Image
+						src={img}
+						height="1000"
+						width="1000"
+						className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+						alt="thumbnail"
+					/>
+				</CardItem>
 
 				<div className="flex justify-between items-center mt-auto">
           <CardItem
