dbedb75a87028334178276ede335c70b5904ea83
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-05-07 07:01:13 -0700

    minor fix

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -51,7 +51,7 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={className}>
-      <div className="my-4">
+      <div className="my-2">
         <div className=" dark:text-white text-black leading-snug tracking-wide">
           {renderWords()}
         </div>
