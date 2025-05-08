c7e58f9aa969ac0758b2c7a9c2ad3f2e425a304a
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-05-07 17:39:59 -0700

    updated skills/tools margins/padding

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
       <h1 className="text-center font-bold text-[32px] md:text-3xl lg:text-4xl mb-8">
         Current <span className="text-[#d1b3ff]">Technologies</span>
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
         {skills.map((skill, index) => (
           <div key={index} className="break-inside-avoid flex flex-row items-center w-full h-18 rounded-xl border-zinc-700 bg-[linear-gradient(180deg,#27272a,#18181b)]">
             <span className="mx-4">{skill.icon}</span>
