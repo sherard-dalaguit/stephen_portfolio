1b78a7d2df1d8c3c6aab7a54366c9c82a35a8631
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-05-12 13:48:18 -0700

    update projects margins/padding

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,11 +11,16 @@ const Projects = ({ limit } : { limit?: number }) => {
 		: projects;
 	return (
 		<div id="projects" className="flex flex-col items-center max-w-screen-xl mx-auto pt-20 pb-40 px-4">
-			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl mb-20">
+			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl">
 				My <span className="text-[#d1b3ff]">Personal Projects</span>
 			</h1>
-			<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-12 gap-4 justify-items-stretch w-full">
-				{projectsToShow.map(({id, title, subheader, des, img, iconLists, githubLink, deployedLink }) => (
+			<h1 className="mx-auto mt-8 text-center text-lg lg:text-xl mb-20">
+				I&apos;ve built a variety of innovative projects that showcase my technical expertise and creative design.
+				<br />
+				Explore my portfolio to see examples of my work.
+			</h1>
+			<section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6 xl:gap-12 items-stretch md:px-4">
+				{projectsToShow.map(({id, title, subheader, des, img, iconLists, githubLink, deployedLink}) => (
 					<ProjectItems
 						key={id}
 						title={title}
@@ -33,7 +38,7 @@ const Projects = ({ limit } : { limit?: number }) => {
 				<Link href="/projects" className="mt-8">
 					<MagicButton
 						title="All Projects"
-						icon={<IconCode />}
+						icon={<IconCode/>}
 						position="right"
 					/>
 				</Link>
