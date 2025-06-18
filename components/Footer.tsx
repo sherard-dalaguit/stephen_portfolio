ce0826d6a49746fced17156ce872a33bd18f2086
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-18 10:45:23 -0700

    fix footer section mobile responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import {socialMedia} from "@/data";
 
 const Footer = () => {
 	return (
-		<footer className="w-full pt-20 pb-10 px-20 relative" id="contact">
+		<footer className="w-full pt-20 pb-10 px-4 sm:px-20 relative" id="contact">
 			<div className="h-48 mb-0 overflow-hidden">
 				<SpotlightReverse/>
 			</div>
@@ -22,11 +22,11 @@ const Footer = () => {
 			</div>
 
 			<div className="flex flex-col items-center max-w-screen-xl mx-auto w-full md:px-4">
-				<h1 className="text-center font-bold text-4xl md:text-5xl lg:text-6xl">
+				<h1 className="text-center font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
 					Interested in <span className="text-[#d1b3ff]">networking</span> or <span
 					className="text-[#d1b3ff]">collaborating</span> on tech projects?
 				</h1>
-				<p className="text-white-200 md:mt-10 my-5 lg:text-lg text-center">Feel free to reach out! I&apos;m eager to network and
+				<p className="text-white-200 md:mt-10 my-5 text-md lg:text-lg text-center">Feel free to reach out! I&apos;m eager to network and
 					explore opportunities to grow together.</p>
 				<a href="mailto:sherarddalaguit.dev@gmail.com">
 					<MagicButton title="Let's get in touch" icon={<IconLocationFilled width={15}/>} position="right"/>
@@ -36,7 +36,7 @@ const Footer = () => {
 			<div className="flex mt-16 md:flex-row flex-col max-w-screen-xl mx-auto justify-between items-center">
 				<p className="md:text-base text-sm md:font-normal font-light">Copyright © 2025 Sherard</p>
 
-				<div className="flex items-center justify-center md:gap-3 gap-6 sm:mt-5 xs:mt-5">
+				<div className="flex items-center justify-center md:gap-3 gap-6 mt-5">
 					{socialMedia.map((profile) => (
 						<a key={profile.id} href={profile.link} target="_blank" rel="noopener noreferrer">
 							<div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
