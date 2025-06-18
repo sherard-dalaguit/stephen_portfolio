65f953751e762756ddcb9c44bdfb9c01d8904106
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-18 10:51:25 -0700

    fix project section mobile responsiveness

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const Projects = () => {
 	return (
-		<div id="projects" className="flex flex-col items-center max-w-screen-xl mx-auto pt-20 pb-40 px-4">
+		<div id="projects" className="flex flex-col items-center max-w-screen-xl mx-auto pt-20 sm:pb-40 px-4">
 			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl">
-				My <span className="text-[#d1b3ff]">Personal Projects</span>
+				My <span className="text-[#d1b3ff]">Projects</span>
 			</h1>
-			<h1 className="mx-auto mt-8 text-center text-lg lg:text-xl mb-20">
+			<h1 className="mx-auto mt-8 text-center text-md sm:text-lg lg:text-xl mb-8 sm:mb-20">
 				I&apos;ve built a variety of innovative projects that showcase my technical expertise and creative design.
 				<br />
-				Explore my portfolio to see examples of my work.
+				Here are some examples of my work so far.
 			</h1>
 			<section>
 				<ProjectDetails>
-					{projects.map(({id, title, subheader, des, img, iconLists, githubLink, deployedLink}) => (
+					{projects.map(({id, title, subheader, des, img, iconLists, githubLink, deployedLink, delayed}) => (
 						<ProjectItems
 							id={id}
 							key={id}
@@ -26,6 +26,7 @@ const Projects = () => {
 							iconLists={iconLists}
 							githubLink={githubLink}
 							deployedLink={deployedLink}
+							delayed={delayed}
 						/>
 					))}
 				</ProjectDetails>
