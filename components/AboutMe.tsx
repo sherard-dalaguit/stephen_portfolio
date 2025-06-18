d65875e86fac5ff636b7cced58eb868a116ca114
sherard-dalaguit <sherard.softwaredev@gmail.com>
2025-06-18 09:48:26 -0700

    fix about section mobile responsiveness

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,4 @@
 import Skills from "@/components/Skills";
-import Tools from "@/components/Tools";
 import Image from "next/image";
 import Link from "next/link";
 import {IconBrandGithub, IconBrandLinkedin, IconFileDescription, IconMail, IconMapPinFilled} from "@tabler/icons-react";
@@ -7,11 +6,11 @@ import React from "react";
 
 const AboutMe = () => {
 	return (
-		<div id="about" className="mx-auto w-full pt-20 pb-40 px-8 md:px-20">
+		<div id="about" className="mx-auto w-full pt-20 sm:pb-40 px-8 md:px-20">
 			<h1 className="text-center font-bold text-[40px] md:text-5xl lg:text-6xl">
 				About <span className="text-[#d1b3ff]">Me</span>
 			</h1>
-			<div className="grid grid-cols-1 lg:grid-cols-2 items-start gap-y-8 lg:gap-x-6 mx-auto max-w-[80vw] xl:max-w-[67vw] mt-4">
+			<div className="grid grid-cols-1 lg:grid-cols-2 items-start gap-y-8 lg:gap-x-6 mx-auto max-w-[80vw] xl:max-w-[67vw] sm:mt-4">
 				<div className="flex flex-col md:flex-row items-center lg:items-start h-full my-4">
 					<div className="w-[200px] h-[200px] flex-shrink-0 relative mb-6">
 						<Image
@@ -23,33 +22,36 @@ const AboutMe = () => {
 						/>
 					</div>
 
-					<div className="flex flex-col md:pl-8 mt-2">
-						<h1 className="text-xl md:text-2xl lg:text-3xl font-bold mt-4">
+					<div className="flex flex-col items-center sm:items-start md:pl-8 mt-2">
+						<h1 className="text-2xl lg:text-3xl font-bold mt-4">
 							Sherard Dalaguit
 						</h1>
-						<h1 className="text-md lg:text-lg text-zinc-400">
+						<h2 className="text-md lg:text-lg text-zinc-400">
 							Full-Stack Software Engineer
-						</h1>
+						</h2>
 
 						<div className="flex flex-row items-center mt-2">
 							<IconMapPinFilled className="text-[#d1b3ff]" />
-							<h1 className="text-md lg:text-lg ml-2">
-								Los Angeles, California&nbsp;•&nbsp;Manila, Philippines
-							</h1>
+							<p className="text-md lg:text-lg ml-2">
+								Los Angeles, California
+								<br className="block sm:hidden" />
+								<span className="hidden sm:inline"> &bull; </span>
+								Manila, Philippines
+							</p>
 						</div>
 
-						<h1 className="text-sm lg:text-md text-zinc-400 mt-2">
+						<p className="text-sm lg:text-md text-zinc-400 mt-2">
 							Computer Science & Software Engineering<br /> at the University of California, Irvine
-						</h1>
+						</p>
 
-						<div className="flex flex-row items-center mt-4">
+						<div className="flex flex-row items-center space-x-2 mt-4">
 							<Link
 								href="https://www.linkedin.com/in/sherard-d"
 								target="_blank"
 								rel="noopener noreferrer"
 								aria-label="LinkedIn profile"
 							>
-								<IconBrandLinkedin className="h-8 w-8 text-neutral-500 dark:text-neutral-300" />
+								<IconBrandLinkedin className="h-9 w-9 text-neutral-500 dark:text-neutral-300" />
 							</Link>
 							<Link
 								href="https://www.github.com/sherard-dalaguit"
@@ -57,27 +59,28 @@ const AboutMe = () => {
 								rel="noopener noreferrer"
 								aria-label="GitHub profile"
 							>
-								<IconBrandGithub className="h-8 w-8 text-neutral-500 dark:text-neutral-300" />
+								<IconBrandGithub className="h-9 w-9 text-neutral-500 dark:text-neutral-300" />
 							</Link>
 							<Link
 								href="mailto:sherarddalaguit.dev@gmail.com"
 								aria-label="Email me"
 							>
-								<IconMail className="h-8 w-8 text-neutral-500 dark:text-neutral-300" />
+								<IconMail className="h-9 w-9 text-neutral-500 dark:text-neutral-300" />
 							</Link>
 							<Link
 								href="https://drive.google.com/file/d/1JGZ10xseERVZ6v3PkFqpF4MdQ90NN2Dr/view"
+								// href="https://drive.google.com/file/d/1zlTZ-HZOx8RJT1mkQE3g2qmn7pPiZxlW/view"
 								target="_blank"
 								rel="noopener noreferrer"
 								aria-label="Resume"
 							>
-								<IconFileDescription className="h-8 w-8 text-neutral-500 dark:text-neutral-300" />
+								<IconFileDescription className="h-9 w-9 text-neutral-500 dark:text-neutral-300" />
 							</Link>
 						</div>
 					</div>
 				</div>
 
-				<ul className="list-disc list-inside text-lg lg:text-xl my-8 space-y-4">
+				<ul className="list-disc list-inside text-md md:text-lg lg:text-xl md:my-8 space-y-4 hidden sm:block">
 					<li>
 						I am a Software Engineer with a strong background in developing innovative web solutions.
 					</li>
@@ -93,7 +96,6 @@ const AboutMe = () => {
 				</ul>
 			</div>
 			<Skills/>
-			<Tools/>
 		</div>
 	)
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,17 +2,17 @@ import {skills} from "@/data";
 
 const Skills = () => {
   return (
-    <section id="skills" className="max-w-screen-xl mx-auto pt-8 pb-15 xl:px-20">
+    <section id="skills" className="max-w-screen-xl mx-auto py-16 xl:px-20">
       <h1 className="text-center font-bold text-[32px] md:text-3xl lg:text-4xl mb-8">
         Current <span className="text-[#d1b3ff]">Technologies</span>
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+      <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
         {skills.map((skill, index) => (
-          <div key={index} className="break-inside-avoid flex flex-row items-center w-full h-18 rounded-xl border-zinc-700 bg-[linear-gradient(180deg,#27272a,#18181b)]">
+          <div key={index} className="break-inside-avoid flex flex-col sm:flex-row items-center p-2 w-full md:h-18 rounded-xl border-zinc-700 bg-[linear-gradient(180deg,#27272a,#18181b)]">
             <span className="mx-4">{skill.icon}</span>
             <div className="flex flex-col">
-              <h1 className="text-lg">{skill.name}</h1>
-              <h1 className="text-sm text-gray-400">{skill.des}</h1>
+              <h1 className="text-md sm:text-lg">{skill.name}</h1>
+              <h1 className="text-sm text-gray-400 hidden sm:block">{skill.des}</h1>
             </div>
           </div>
         ))}
