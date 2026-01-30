a84ab04a77446112066ba04c2084ee81417007f0
sherard-dalaguit <sherard.softwaredev@gmail.com>
2026-01-29 20:31:45 -0800

    update copyright year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
 			</div>
 
 			<div className="flex mt-16 md:flex-row flex-col max-w-screen-xl mx-auto justify-between items-center">
-				<p className="md:text-base text-sm md:font-normal font-light">Copyright © 2025 Sherard</p>
+				<p className="md:text-base text-sm md:font-normal font-light">Copyright © 2026 Sherard</p>
 
 				<div className="flex items-center justify-center md:gap-3 gap-6 mt-5">
 					{socialMedia.map((profile) => (
